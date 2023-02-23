7f81167e45943436eb6708c28e7334045b00733e
aom19 <39848284+aom19@users.noreply.github.com>
2023-02-23 18:24:02 +0200

    update the script to verify if admin|user already exists

--- create_superuser.sh
+++ create_superuser.sh
@@ -7,8 +7,17 @@ python manage.py collectstatic --noinput
 # Create database tables
 echo "Apply database migrations"
 python manage.py migrate --noinput
+# Check if superuser exists
+echo "Check if superuser exists"
+if ! python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='myadmin').exists())"; then
+    echo "Error checking for existing superuser"
+    exit 1
+fi
 
 # Create superuser
-echo "Create superuser"
-echo "from django.contrib.auth import get_user_model; User = get_user_model();
- User.objects.create_superuser('admin', 'admin@example.com', '$(echo $SUPERUSER_PASSWORD)')" |  python manage.py shell
+if [ "$(python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='myadmin').exists())")" = "False" ]; then
+    echo "Create superuser"
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('myadmin', 'myadmin@example.com', '$(echo $SUPERUSER_PASSWORD)')" | python manage.py shell
+else
+    echo "Superuser already exists"
+fi
